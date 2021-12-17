c5c04c7f1edebf456510f3ea3f26a5c669242ae1
Edwin Jimenez Gomez <erjimene@unal.edu.co>
2021-12-17 04:56:11 -0500

    C2
    Agregando código
    
    Co-Authored-By: jekamelo <92174216+jekamelo@users.noreply.github.com>
    Co-Authored-By: Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>

--- src/uiMain/Juego.java
+++ src/uiMain/Juego.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import artefactos.Objeto;
+import artefactos.Tesoro;
+import complementario.Habilidad;
 import complementario.TipoDeObjeto;
+import complementario.TipoEfecto;
+import complementario.TipoHabilidad;
 import personajes.*;
 
 public class Juego {
@@ -135,6 +139,144 @@ System.out.println("¿Deseas empezar un juego nuevo o cargar la partida guardada?
 		materiales3.add(material3);
 		materiales3.add(material1);
 		materiales3.add(material8);
+		
+		arcoT1.setMaterialesEvolucion(materiales3);
+		espadaT1.setMaterialesEvolucion(materiales2);
+		varitaT1.setMaterialesEvolucion(materiales1);
+		armaduraM.setMaterialesEvolucion(materiales3);
+		anilloM.setMaterialesEvolucion(materiales1);
+		
+		
+		arcoT1.setEvolucion(arcoT2);
+		espadaT1.setEvolucion(espadaT2);
+		varitaT1.setEvolucion(varitaT2);
+		anilloM.setEvolucion(anilloP);
+		armaduraM.setEvolucion(armaduraP);
+		
+		
+		ArrayList<Objeto> premios1 = new ArrayList<Objeto>();
+		ArrayList<Objeto> premios2 = new ArrayList<Objeto>();
+		ArrayList<Objeto> premios3 = new ArrayList<Objeto>();
+		
+		premios1.add(armaduraL);
+		premios1.add(material5);
+		premios1.add(lanzaT);
+		premios1.add(material3);
+		premios1.add(material7);
+		premios1.add(armaH2);
+		premios1.add(armaH5);
+		premios1.add(anilloL);
+		
+		
+		premios2.add(armaduraM);
+		premios2.add(material8);
+		premios2.add(lanzaT);
+		premios2.add(material4);
+		premios2.add(material1);
+		premios2.add(armaduraH6);
+		premios2.add(armaH3);
+		premios2.add(espadaT1);
+		premios2.add(anilloM);
+		
+		premios3.add(material2);
+		premios3.add(material6);
+		premios3.add(material3);
+		premios3.add(material9);
+		premios3.add(espadaT1);
+		premios3.add(arcoT1);
+		premios3.add(varitaT1);
+		premios3.add(armaduraM);
+		premios3.add(anilloM);
+		
+		
+		// Creacion de tesoros
+		
+		Tesoro tesoro1 = new Tesoro("Vuelo de Onivia",premios1);
+		Tesoro tesoro2 = new Tesoro("Serpiente gris",premios2);
+		Tesoro tesoro3 = new Tesoro("Cofre divino",premios3);
+		
+		
+		
+		// Objetos que vende el herrero
+		ArrayList<Objeto> objetosVenta = new ArrayList<Objeto>();
+		objetosVenta.add(armaH1);
+		objetosVenta.add(armaH2);
+		objetosVenta.add(armaH3);
+		objetosVenta.add(armaH4);
+		objetosVenta.add(armaH5);
+		objetosVenta.add(armaduraH6);
+		
+		
+		// Creacion herrero
+		Herrero pacho = new Herrero("Pacho",objetosVenta,80,15);
+		
+		
+		// Creacion habilidades
+		Habilidad habilidad1 = new Habilidad(TipoHabilidad.DEFENSAFISICA,"Coraza de acero",10,0,3,0, TipoEfecto.NULO, true);
+		Habilidad habilidad2 = new Habilidad(TipoHabilidad.SANACION,"Palabra regeneradora",10,0,3,0, TipoEfecto.NULO, true);
+		Habilidad habilidad3 = new Habilidad(TipoHabilidad.ATAQUEFISICO,"Ira del trueno",10,0,3,2, TipoEfecto.NULO, false);
+		Habilidad habilidad4 = new Habilidad(TipoHabilidad.DEFENSAMAGICA,"Escudo magico",10,0,2,0, TipoEfecto.NULO, true);
+		Habilidad habilidad5 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Tempestad del Zodiaco",10,0,2,0, TipoEfecto.NULO, false);
+		Habilidad habilidad6 = new Habilidad(TipoHabilidad.APOYOFISICO,"Llama interna",10,0,3,0, TipoEfecto.NULO, true);
+		Habilidad habilidad7 = new Habilidad(TipoHabilidad.ATAQUEFISICO,"Nova llameante",10,2,4,5, TipoEfecto.QUEMAR, false);
+		Habilidad habilidad8 = new Habilidad(TipoHabilidad.ATAQUEFISICO,"Salto fuerte",10,0,2,0, TipoEfecto.NULO, false);
+		Habilidad habilidad9 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Mordedura de cobra",13,2,4,8, TipoEfecto.ENVENENAR, false);
+		Habilidad habilidad10 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Flecha venenosa",10,2,3,7, TipoEfecto.ENVENENAR, false);
+		Habilidad habilidad11 = new Habilidad(TipoHabilidad.ATAQUEFISICO,"Furia vikinga",10,0,2,0, TipoEfecto.NULO, false);
+		Habilidad habilidad12 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Bola de fuego",10,2,3,7, TipoEfecto.QUEMAR, false);
+		Habilidad habilidad13 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Frio de invierno",10,1,3,10, TipoEfecto.CONGELAR, false);
+		Habilidad habilidad14 = new Habilidad(TipoHabilidad.APOYOFISICO,"Grito alentador",15,0,3,0, TipoEfecto.NULO, true);
+		Habilidad habilidad15 = new Habilidad(TipoHabilidad.DEFENSAFISICA,"Coraza reforzada",15,0,3,1, TipoEfecto.NULO, false);
+		Habilidad habilidad16 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Cero absoluto",15,1,3,10, TipoEfecto.CONGELAR, false);
+		Habilidad habilidad17 = new Habilidad(TipoHabilidad.ATAQUEMAGICO,"Resplandor del caos",20,1,5,6, TipoEfecto.QUEMAR, false);
+		Habilidad habilidad18 = new Habilidad(TipoHabilidad.SANACION,"Rejuvenecimiento",23,0,3,0, TipoEfecto.NULO, true);
+
+		
+		ArrayList<Habilidad> habilidades = new ArrayList<Habilidad>(); //enemigo nodo1
+		habilidades.add(habilidad1);
+		habilidades.add(habilidad7);
+		habilidades.add(habilidad5);
+		habilidades.add(habilidad4);
+		habilidades.add(habilidad8);
+
+		
+		ArrayList<Habilidad> habilidades1 = new ArrayList<Habilidad>(); // enemigo1 nodo2 
+		habilidades1.add(habilidad15);
+		habilidades1.add(habilidad2);
+		habilidades1.add(habilidad8);
+		habilidades1.add(habilidad13);
+		habilidades1.add(habilidad9);
+		
+		
+		
+		ArrayList<Habilidad> habilidades2 = new ArrayList<Habilidad>(); // enemigo2 nodo2 
+		habilidades2.add(habilidad15);
+		habilidades2.add(habilidad14);
+		habilidades2.add(habilidad10);
+		habilidades2.add(habilidad2);
+		habilidades2.add(habilidad17);
+		
+		
+		ArrayList<Habilidad> habilidades3 = new ArrayList<Habilidad>(); // enemigo1 nodo3 
+		habilidades3.add(habilidad15);
+		habilidades3.add(habilidad14);
+		habilidades3.add(habilidad10);
+		habilidades3.add(habilidad16);
+		habilidades3.add(habilidad18);
+		
+		
+		ArrayList<Habilidad> habilidades4 = new ArrayList<Habilidad>(); // dioses 1 
+		habilidades4.add(habilidad4);
+		habilidades4.add(habilidad2);
+		habilidades4.add(habilidad14);
+		habilidades4.add(habilidad11);
+		
+		
+		ArrayList<Habilidad> habilidades5 = new ArrayList<Habilidad>(); // dioses 2 
+		habilidades5.add(habilidad4);
+		habilidades5.add(habilidad6);
+		habilidades5.add(habilidad14);
+		habilidades5.add(habilidad15);
 
 	}
 

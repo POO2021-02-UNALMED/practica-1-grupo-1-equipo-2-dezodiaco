0d345807a77039be779813281e17e6d67d4c5fa2
jekamelo <92174216+jekamelo@users.noreply.github.com>
2021-12-17 04:26:46 -0500

    Bienvenida

--- src/uiMain/Juego.java
+++ src/uiMain/Juego.java
@@ -3,7 +3,12 @@ package uiMain;
 public class Juego {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("             Bienvenido al Desafío del Zodiaco               \n"+"\n"+
+				 "Los dioses del zodiaco te han encomendado la tarea de derrotar al Asediador de almas.\n"+ 
+				 "Un ser oscuro inmune al poder sagrado que busca dominar al mundo estableciendo\n"+ 
+				 "un letal ejército de almas pérdidas que ha ido recolectando a lo largo de milenios.\n"+ 
+				 "Solo los humanos pueden detener su progreso, pues estos son hijos de la luz y la oscuridad\n");
 
 	}
 
